322063f05e4a3e90e81572545fc362a17660d775
hava12 <hava12@naver.com>
2022-01-07 00:09:12 +0900

    Initial commit

---
+++ src/main/java/com/filming/userservice/v1/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.filming.userservice.v1.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1")
+@CrossOrigin(origins = "http://localhost:3000")
+public class LoginController {
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Map<String, String> req) {
+        System.out.println("req = " + req);
+        return new ResponseEntity<String>("ok", HttpStatus.ACCEPTED );
+    }
+}
